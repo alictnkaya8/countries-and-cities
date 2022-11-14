e9a4f552a59b48d0639e4d5d5237de7b1c68cbaf
alictnkaya8 <49043825+alictnkaya8@users.noreply.github.com>
2022-11-14 14:17:41 +0300

    db options

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config/dist';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CountriesController } from './countries/countries.controller';
 import { CountriesService } from './countries/countries.service';
@@ -7,6 +9,21 @@ import { Country, CountrySchema } from './countries/country.schema';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017'),
+    // MongooseModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: async (configService: ConfigService) => {
+    //     const username = configService.get('MONGO_USERNAME');
+    //     const password = configService.get('MONGO_PASSWORD');
+    //     const database = configService.get('MONGO_DATABASE');
+    //     const host = configService.get('MONGO_HOST');
+
+    //     return {
+    //       uri: `mongodb://${username}:${password}@${host}`,
+    //       dbname: database,
+    //     };
+    //   },
+    //   inject: [ConfigService],
+    // }),
     MongooseModule.forFeature([{ name: Country.name, schema: CountrySchema }]),
   ],
   controllers: [CountriesController],
